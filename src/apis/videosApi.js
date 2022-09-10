d09917281ae2b61e791769c34ae4eea89a4b70c2
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2022-09-10 07:42:35 +0430

    refactor

--- src/apis/videosApi.js
+++ src/apis/videosApi.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const getVideosRequest = () => {
-  const response = axios.get("fa/v1/video/video/mostViewedVideos");
-  return response;
+  return axios.get("fa/v1/video/video/mostViewedVideos");
 };
